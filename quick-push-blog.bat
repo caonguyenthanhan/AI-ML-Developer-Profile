068ab26401618059e05adee3ff7037ef34ec089e
thành An <caonguyenthanhan.aaa@gmail.com>
2026-04-28 17:03:49 +0700

    chore(blog): fix quick push script and remove stray images

--- quick-push-blog.bat
+++ quick-push-blog.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal
 
 cd /d "%~dp0"
 
@@ -9,20 +9,33 @@ echo ==========================================
 echo.
 
 set "MSG=%~1"
-if "%MSG%"=="" (
-  set /p MSG=Nhap commit message (mac dinh: update blog): 
-)
-if "!MSG!"=="" (
-  set "MSG=update blog"
-)
+set "SHOULD_PAUSE=1"
+if not "%~1"=="" set "SHOULD_PAUSE=0"
+
+if "%MSG%"=="" set /p "MSG=Nhap commit message (mac dinh: update blog): "
+if "%MSG%"=="" set "MSG=update blog"
 
 echo.
 echo Dang stage cac file blog...
-git add public\blog public\image vercel.json
+set "POST=%~2"
+git add -A public\blog
+git add vercel.json
+
+if not "%POST%"=="" (
+  if /I "%POST%"=="--all-images" (
+    git add -A public\image
+  ) else (
+    if exist "public\image\%POST%\" (
+      git add -A "public\image\%POST%"
+    ) else (
+      echo Khong tim thay thu muc anh: public\image\%POST%
+    )
+  )
+)
 
 echo.
-echo Dang commit: !MSG!
-git commit -m "!MSG!"
+echo Dang commit: %MSG%
+git commit -m "%MSG%"
 if errorlevel 1 (
   echo.
   echo Khong co thay doi moi de commit hoac commit that bai.
@@ -43,5 +56,5 @@ echo Hoan tat: da commit + push blog len GitHub.
 
 :end
 echo.
-pause
+if "%SHOULD_PAUSE%"=="1" pause
 endlocal
